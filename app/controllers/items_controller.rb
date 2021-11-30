3bef87a95f8360f13314408b97d6f96f2a1f83d1
Anthony Hill <hillganthony@outlook.com>
2021-11-30 09:13:34 -0500

    commit tree 56478bd4b3173a5c30e00dbf3a665cc0929d3c4b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+ 
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    if user = User.find(params[:id])
+    item = Item.create(item_params)
+    render json: item, include: :user, status: :created
+  else
+    item
+      end
+     end
+
+  private
+
+  def item_params
+    params.permit(:name,:description,:user)
+  end
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
